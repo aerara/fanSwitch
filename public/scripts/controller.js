fc75138ec4f38c2a6994ad2102938c02a26dcafb
NerissaSunit <nerissa.sunit0004@gmail.com>
2019-10-11 20:22:06 +0800

    Commit again

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -1,92 +1,86 @@
-// basic functionalities
-function connectFunc(){
-  // console.log("Connecting..");
-  displayMessage("Connecting..");  
-  // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-  client = mqtt.connect(document.getElementById('broker').value)
-  // console.log(document.getElementById('broker').value);
-  displayMessage(document.getElementById('broker').value);
+// // basic functionalities
+// function connectFunc(){
+//   // console.log("Connecting..");
+//   displayMessage("Connecting..");  
+//   // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+//   client = mqtt.connect(document.getElementById('broker').value)
+//   // console.log(document.getElementById('broker').value);
+//   displayMessage(document.getElementById('broker').value);
 
-  client.on("connect", function(){
-    // console.log("Successfully connected");
-    displayMessage("Successfully connected");
-  })
+//   client.on("connect", function(){
+//     // console.log("Successfully connected");
+//     displayMessage("Successfully connected");
+//   })
 
-  client.on("message", function (topic, payload) {
-    // console.log([topic, payload].join(": "));
-    // console.log("Received { topic: " + topic + "; payload: " + payload + " }");
-    displayMessage("Received { topic: " + topic + "; payload: " + payload + " }");
-    // client.end();
-  })
-}
-
-// function disconnectFunc(){
-//   // console.log("subscribe");
-//   // client.subscribe("mqtt/demo");
-//   // client = mqtt.connect(document.getElementById('broker').value)
-//   // console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + " }");
-//   displayMessage(document.getElementById('broker').value + " has been disconnected.")
+//   client.on("message", function (topic, payload) {
+//     // console.log([topic, payload].join(": "));
+//     // console.log("Received { topic: " + topic + "; payload: " + payload + " }");
+//     displayMessage("Received { topic: " + topic + "; payload: " + payload + " }");
+//     // client.end();
+//   })
 // }
 
-function publishFunc(){
-  // console.log("publish");
-  // client.publish("mqtt/demo", "hello world!");
-  client.publish(document.getElementById('pub-topic').value, document.getElementById('pub-payload').value)
-  // console.log("Published { topic: " + document.getElementById('pub-topic').value + "; payload: " + document.getElementById('pub-payload').value + " }");
-  displayMessage("Published { topic: " + document.getElementById('pub-topic').value + "; payload: " + document.getElementById('pub-payload').value + " }");
-  // console.log(document.getElementById('pub-topic').value);
-  // console.log(document.getElementById('pub-payload').value);
-}
+// // function disconnectFunc(){
+// //   // console.log("subscribe");
+// //   // client.subscribe("mqtt/demo");
+// //   // client = mqtt.connect(document.getElementById('broker').value)
+// //   // console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + " }");
+// //   displayMessage(document.getElementById('broker').value + " has been disconnected.")
+// // }
 
-// function unpublishFunc(){
+// function publishFunc(){
 //   // console.log("publish");
 //   // client.publish("mqtt/demo", "hello world!");
-//   client.unpublish(document.getElementById('pub-topic').value, document.getElementById('pub-payload').value)
+//   client.publish(document.getElementById('pub-topic').value, document.getElementById('pub-payload').value)
 //   // console.log("Published { topic: " + document.getElementById('pub-topic').value + "; payload: " + document.getElementById('pub-payload').value + " }");
-//   displayMessage("Unpublished { topic: " + document.getElementById('pub-topic').value + " }");
+//   displayMessage("Published { topic: " + document.getElementById('pub-topic').value + "; payload: " + document.getElementById('pub-payload').value + " }");
 //   // console.log(document.getElementById('pub-topic').value);
 //   // console.log(document.getElementById('pub-payload').value);
 // }
 
-function subscribeFunc(){
-  // console.log("subscribe");
-  // client.subscribe("mqtt/demo");
-  client.subscribe(document.getElementById('sub-topic').value);
-  // console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + " }");
-    displayMessage("Subscribe { topic: " + document.getElementById('sub-topic').value + " }")
-}
+// // function unpublishFunc(){
+// //   // console.log("publish");
+// //   // client.publish("mqtt/demo", "hello world!");
+// //   client.unpublish(document.getElementById('pub-topic').value, document.getElementById('pub-payload').value)
+// //   // console.log("Published { topic: " + document.getElementById('pub-topic').value + "; payload: " + document.getElementById('pub-payload').value + " }");
+// //   displayMessage("Unpublished { topic: " + document.getElementById('pub-topic').value + " }");
+// //   // console.log(document.getElementById('pub-topic').value);
+// //   // console.log(document.getElementById('pub-payload').value);
+// // }
 
-// function unsubscribeFunc(){
+// function subscribeFunc(){
 //   // console.log("subscribe");
 //   // client.subscribe("mqtt/demo");
-//   client.unsubscribe(document.getElementById('sub-topic').value);
+//   client.subscribe(document.getElementById('sub-topic').value);
 //   // console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + " }");
-//   displayMessage("Unsubscribe { topic: " + document.getElementById('sub-topic').value + " }");
+//     displayMessage("Subscribe { topic: " + document.getElementById('sub-topic').value + " }")
 // }
 
-function off(){
-  document.getElementById("messages").innerHTML = "The fan is currently turned off";
-  client.subscribe(document.getElementById('off').value);
-  displayMessage("Subscribe { topic: " + document.getElementById('off').value + " }")
-}
+// // function unsubscribeFunc(){
+// //   // console.log("subscribe");
+// //   // client.subscribe("mqtt/demo");
+// //   client.unsubscribe(document.getElementById('sub-topic').value);
+// //   // console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + " }");
+// //   displayMessage("Unsubscribe { topic: " + document.getElementById('sub-topic').value + " }");
+// // }
 
-function uno(){
-  document.getElementById("messages").innerHTML = "The fan is currently turned on at 1";
-  client.subscribe(document.getElementById('off').value);
-  displayMessage("Subscribe { topic: " + document.getElementById('off').value + " }")
+// // function displayMessage(message) {
+// //   var node = document.createElement("li");
+// //   var textnode = document.createTextNode(message);
+// //   node.appendChild(textnode);
+// //   document.getElementById("messages").appendChild(node);
+// // }
 
-}
-
-function dos(){ 
-  document.getElementById("messages").innerHTML = "The fan is currently turned on at 2";}
-
-function tris(){
-  document.getElementById("messages").innerHTML = "The fan is currently turned on at 3";
-}
-
-function displayMessage(message) {
-  var node = document.createElement("li");
-  var textnode = document.createTextNode(message);
-  node.appendChild(textnode);
-  document.getElementById("messages").appendChild(node);
-}
+$(document).ready(function () {
+	var text = "The fan is turned ";
+	client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt");
+	client.on("connect", function () {console.log("Connected Successfully.")})
+	$('button').on('click', function (e) {
+		$('p').html(text + $(this).val())
+    event.preventDefault();
+    var date = new Date();
+    var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+		client.publish("aira/fan/status", $('p').html() +" "  + time);
+		console.log("Topic: aira/fan/status", "Payload: " + $('p').html() +" "+ time);
+	})
+})
